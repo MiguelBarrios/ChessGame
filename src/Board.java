1ffc281df134b86e9a5fc3a389c69401a3905a06
alex <alexsacafe@gmail.com>
2019-06-15 19:05:36 -0500

    ui update

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,26 @@
-public class Board
-{
+import java.awt.Color;
+import java.awt.GridLayout;
 
+import javax.swing.JPanel;
+
+public class Board extends JPanel
+{
+    private Tile[][] tiles;
+
+    public Board() {
+        super();
+        GridLayout grid = new GridLayout(8, 8);
+        this.setLayout(grid);
+        tiles = new Tile[8][8];
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                tiles[row][col] = new Tile(row, col);
+                this.add(tiles[row][col]);
+            }
+        }
+    }
+
+    public Tile getTile(int row, int col) {
+        return tiles[row][col];
+    }
 }
